281ad3b9c5a949f152ffb9acf58d379bca0c4361
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-07-09 12:38:47 -0400

    Add and control form

--- src/app/hacker-news/hacker-news.module.ts
+++ src/app/hacker-news/hacker-news.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   exports: [
     HomeComponent
--- src/app/hacker-news/home/home.component.ts
+++ src/app/hacker-news/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HackerNewsService } from '../hacker-news.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public frameworks: any[] = [
+    { value: '', label: 'Select your news' },
+    { value: 'angular', label: 'Angular' },
+    { value: 'reactjs', label: 'Reactjs' },
+    { value: 'vuejs', label: 'Vuejs' },
+  ]
 
-  ngOnInit(): void {
+  public myForm = this.fb.group({
+    framework: ['']
+  })
+
+  constructor(private dataService: HackerNewsService,
+              private fb: FormBuilder) { }
+
+  ngOnInit(): void { }
+
+  getFrameworkNews(framework: string, page: number) {
+    this.dataService.getData(framework, page).subscribe(res => {
+      console.log(res);
+    })
+  }
+
+  showData() {
+    const framework = this.myForm.controls.framework.value;
+    if (framework) {
+      this.getFrameworkNews(framework, 1);
+    }
   }
 
 }
