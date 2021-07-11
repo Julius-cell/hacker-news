0c16ac5856b6fdb24ed865af324ca044faf95414
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-07-11 02:26:21 -0400

    Update data service

--- src/app/hacker-news/hacker-news.service.ts
+++ src/app/hacker-news/hacker-news.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
+import { New } from './model/new';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,12 @@ export class HackerNewsService {
 
   private base_url = environment.base_url;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getData(framework: string, page: number): Observable<any> {
+  getNewsByPage(framework: string, page: number): Observable<New[]> {
     const query = `search_by_date?query=${framework}&page=${page}`;
     return this.http.get<any>(`${this.base_url}${query}`).pipe(
-      map(({hits, ...rest}) => hits)
+      map(({hits, ...rest}) => hits),
     );
   }
 }
